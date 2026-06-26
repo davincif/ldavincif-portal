8a4e296223d0966f43e0486c08fd1905cf1cd4ef
davincif <ldavincif@gmail.com>
2026-06-26 11:32:19 +0100

    header done

---
+++ src/components/header.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import HexagonIcon from '@/icons/hexagonIcon.vue';
+import { ref } from 'vue';
+
+const menus = ref([
+  {
+    name: 'About',
+    achor: '#about',
+  },
+  {
+    name: 'Exprience',
+    achor: '#exprience',
+  },
+  {
+    name: 'Work',
+    achor: '#work',
+  },
+  {
+    name: 'Contact',
+    achor: '#contact',
+  },
+]);
+</script>
+
+<template>
+  <div class="flex items-center justify-between gap-2">
+    <HexagonIcon :size="50" label="DV" />
+
+    <span class="flex gap-12">
+      <a
+        v-for="(menu, index) in menus"
+        class="text-over-bg-flashy text-lg font-semibold hover:cursor-pointer"
+      >
+        <span class="text-cta font-bold">
+          {{ String(index + 1).padStart(2, '0') }}.
+        </span>
+        {{ menu.name }}
+      </a>
+    </span>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/icons/hexagonIcon.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps({
+  size: {
+    type: Number,
+    default: 200,
+  },
+  strokeWidth: {
+    type: Number,
+    default: 12,
+  },
+  color: {
+    type: String,
+    default: 'cta',
+  },
+  label: {
+    type: String,
+    default: '✟♥',
+  },
+});
+
+const points = computed(() => {
+  // viewBox size
+  const s = 200;
+
+  // Create a taller hexagon by adjusting the Y coordinates
+  return (
+    // top point
+    `${s / 2},10 ` +
+    // top right
+    `${s - 20},${s * 0.25} ` +
+    // bottom right
+    `${s - 20},${s * 0.75} ` +
+    // bottom point
+    `${s / 2},${s - 10} ` +
+    // bottom left
+    `20,${s * 0.75} ` +
+    // top left
+    `20,${s * 0.25}`
+  );
+});
+</script>
+
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    viewBox="0 0 200 200"
+    :class="`text-${color}`"
+  >
+    <polygon
+      :points="points"
+      fill="none"
+      stroke="currentColor"
+      :stroke-width="strokeWidth"
+    />
+    <text
+      x="100"
+      y="110"
+      text-anchor="middle"
+      dominant-baseline="middle"
+      fill="currentColor"
+      class="text-7xl font-bold"
+    >
+      {{ label }}
+    </text>
+  </svg>
+</template>
+
+<style scoped></style>
